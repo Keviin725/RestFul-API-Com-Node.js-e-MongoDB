0661e5b59d0c47eaaa0df56a4e89f0aaaa98c1e5
Kevin Machava <kevinmachava20@gmail.com>
2023-01-25 23:37:58 +0200

    Create Person.js

---
+++ models/Person.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const Person = mongoose.model('Person', {
+    name: String,
+    age: Number,
+    cellphone: Number,
+    address: String,
+    approved: Boolean
+})
+
+module.exports = Person
