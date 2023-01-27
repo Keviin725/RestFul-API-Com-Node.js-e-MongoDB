cd22fa8f189ade189b424af79e5fdffd226c032f
Kevin Machava <kevinmachava20@gmail.com>
2023-01-27 14:59:36 +0200

    update

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 const app = express()
 
 // Ler o JSON / midddlewares
@@ -26,10 +24,11 @@ app.get('/', (req, res) =>{
     res.json({message: 'First Request!'})
 })
 
-
-// Conectar a BD
+// Credentials
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+
+// Conectar a BD
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@clusterapi.pk8p6ev.mongodb.net/?retryWrites=true&w=majority`)
 .then(() =>{
     console.log("Rocket Launched Successfully! 🚀")
