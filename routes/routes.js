c77ed3c1f9d811eee98acd67bda138837f5a651d
Kevin Machava <kevinmachava20@gmail.com>
2023-02-09 16:23:00 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -122,6 +122,16 @@ function checkToken(req, res, next){
     if(!token){
         return res.status(401).json({message: 'Acesso negado'})
     }
+
+    try {
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+        
+        next()
+        
+    } catch (error) {
+        res.status(400).json({message: 'Token Invalido'})
+    }
 }
 
 
